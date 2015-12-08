b299a9f9e8da42948997e0e74cb7d2fde50fe0bc
Antonio Aloisio <gnuton@gnuton.org>
2015-12-08 23:41:57 +0200

    Commenting out GSM dependency

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@ TARGET_KERNEL_SOURCE := kernel/goldfish
 TARGET_KERNEL_CONFIG := cyanogenmod_goldfish_armv7_defconfig
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
